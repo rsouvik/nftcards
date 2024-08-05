7fd495aab3386495ba24643b1638e450ffb9f018
rsouvik <sales@getmypickly.com>
2024-08-05 15:33:41 -0700

    changing fetch parameters for opensea

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../contexts/CartContext';
 import { CartItem } from "../types";
 import axios from 'axios';
